7fa1fa62a55bc2f5e89b6f050dbf677195c38e7c
tidalboot <nickjones@Nicks-MacBook-Pro-3.local>
2017-09-04 11:56:57 +0100

    initial commit

---
+++ Telescope/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Telescope
+//
+//  Created by Nick Jones on 04/09/2017.
+//  Copyright © 2017 NickJones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
