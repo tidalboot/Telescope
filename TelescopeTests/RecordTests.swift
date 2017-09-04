3b591eee13e200a7466e4674c2f4708571281de7
tidalboot <nickjones@Nicks-MacBook-Pro-3.local>
2017-09-04 12:44:28 +0100

    Added all of the tests that we need to get working for us to ensure our model will be ready to use

--- TelescopeTests/RecordTests.swift
+++ TelescopeTests/RecordTests.swift
@@ -1,7 +1,4 @@
-//
-//  RecordTests.swift
-//  Telescope
-//
+
 //  Created by Nick Jones on 04/09/2017.
 //  Copyright © 2017 NickJones. All rights reserved.
 //
@@ -26,22 +23,101 @@ class RecordTests: XCTestCase {
             }
         }
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    func testMockedRecordsDataHasLoaded() {
+        XCTAssertNotNil(mockedRecordsData, "Mock records data was not loaded correctly")
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testParseRecordsFromRawDataReturnsAnArrayOfRecords() {
+        let records = TelescopeRecord.parseRecords(fromRawData: mockedRecordsData)
+        XCTAssert(records.isEmpty!, "Unable to parse an array of records from the mocked  data")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testDateTakenIsReturnedInTheCorrectFormat() {
+        let records = TelescopeRecord.parseRecords(fromRawData: mockedRecordsData)
+        
+        if (records.isEmpty) {
+            XCTFail("No records were found")
         }
+        
+        XCTAssertEqual(
+            records.first.DateTaken,
+            "3rd September 2017",
+            "Record date taken is not being parsed to the correct format"
+        )
     }
     
+    func testImageDimensionsAreInTheCorrectFormatWhenAvailable() {
+        let records = TelescopeRecord.parseRecords(fromRawData: mockedRecordsData)
+        
+        if (records.isEmpty) {
+            XCTFail("No records were found")
+        }
+        
+        XCTAssertEqual(
+            records.first.Dimensions,
+            "Dimensions: 240x180",
+            "Dimensions property is not being extracted when available"
+        )
+    }
+    
+    func testDefaultImageDimensionsValueIsCorrectWhenDimensionsAreUnavailable() {
+        let records = TelescopeRecord.parseRecords(fromRawData: mockedRecordsData)
+        
+        if (records.isEmpty) {
+            XCTFail("No records were found")
+        }
+        
+        XCTAssertEqual(
+            records[1].Dimensions,
+            "Dimensions: N/A",
+            "Dimensions property is not being set to the correct default value when dimensions are not available"
+        )
+    }
+    
+    func testAuthorIsRetrievedWhenAvailable() {
+        let records = TelescopeRecord.parseRecords(fromRawData: mockedRecordsData)
+        
+        if (records.isEmpty) {
+            XCTFail("No records were found")
+        }
+    
+        XCTAssertEqual(
+            records.first.Author,
+            "TyrionFordring",
+            "Author property is not being extracted when available"
+        )
+    }
+
+    
+    func testDefaultAuthorValueIsCorrectWhenAuthorIsUnavailable() {
+        let records = TelescopeRecord.parseRecords(fromRawData: mockedRecordsData)
+        
+        if (records.isEmpty) {
+            XCTFail("No records were found")
+        }
+        
+        XCTAssertEqual(
+            records[1].Author,
+            "N/A",
+            "Author property is not being set to the correct default value when the author is not available"
+        )
+    }
+    
+    func testOriginalSizedImageURLIsCorrectlyInferredFromMediumImageURL() {
+        let records = TelescopeRecord.parseRecords(fromRawData: mockedRecordsData)
+        
+        if (records.isEmpty) {
+            XCTFail("No records were found")
+        }
+        
+        XCTAssertEqual(
+            records.first.Images.Original,
+            "https://farm5.staticflickr.com/4353/36184066574_e352dc6bd7.jpg",
+            "Original sized image URL is not being inferred from the default provided medium image URL")
+    }
 }
+
+
+
+
